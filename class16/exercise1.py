29ba07d1352fa06f3ba0a550552c0df5585e8ac9
Matheus Rian <souzamatheusrian@gmail.com>
2023-04-20 09:28:10 -0300

    feat: exercise1 of class16

---
+++ class16/exercise1.py
@@ -0,0 +1,9 @@
+phrase = 'TypeScript rainha, python nadinha'
+
+print(phrase.upper())
+print(phrase.lower())
+print(phrase.capitalize())
+print(phrase.title())
+print(phrase.count('a'))
+new_phrase = phrase.replace('nadinha', ' ')
+print(new_phrase)
